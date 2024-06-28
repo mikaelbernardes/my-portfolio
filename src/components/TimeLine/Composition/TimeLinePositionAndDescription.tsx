db8942985415c4546ee612820cdbf94d5cf7fe2b
Mikael <bernardesmikael@outlook.com>
2024-06-27 21:05:42 -0300

    feat: add TimeLinePositionAndDescription component that makes up TimeLine component

---
+++ src/components/TimeLine/Composition/TimeLinePositionAndDescription.tsx
@@ -0,0 +1,29 @@
+import { useLanguageStore } from "@/factory/STORE_FACTORY/impls";
+
+import { Experience } from "../types";
+
+interface TimeLinePositionAndDescription {
+  experience: Experience;
+  descriptionRef: React.RefObject<HTMLParagraphElement>;
+}
+
+function TimeLinePositionAndDescription({
+  descriptionRef,
+  experience,
+}: TimeLinePositionAndDescription) {
+  const { language } = useLanguageStore();
+  return (
+    <div className="flex flex-col -mt-1">
+      <h4 className="font-medium lg:text-[26px] txt-100">
+        {language === "en" ? experience.positionEN : experience.positionPT}
+      </h4>
+      <p ref={descriptionRef} className="font-light lg:text-base txt-300 w-80">
+        {language === "en"
+          ? experience.descriptionEN
+          : experience.descriptionPT}
+      </p>
+    </div>
+  );
+}
+
+export { TimeLinePositionAndDescription };

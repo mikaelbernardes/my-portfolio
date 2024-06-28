e458448cbb6f2685fb0598f5f83cd6d381420552
Mikael <bernardesmikael@outlook.com>
2024-06-27 21:06:27 -0300

    feat: add Experience interface that makes up TimeLine component

---
+++ src/components/TimeLine/types.d.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+  company: string;
+  datePT: string;
+  dateEN: string;
+  positionPT: string;
+  positionEN: string;
+  descriptionPT: string;
+  descriptionEN: string;
+}

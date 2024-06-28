ea51a021eeeb9a5615bd37ed0b56b7061da0be26
Mikael <bernardesmikael@outlook.com>
2024-06-27 21:07:40 -0300

    feat: add TimeLine data that makes up TimeLine component

---
+++ src/app/_sections/ResumeData.ts
@@ -0,0 +1,59 @@
+import { Experience } from "@/components/TimeLine/types";
+
+export const experiencesData: Experience[] = [
+  {
+    company: "Cartsys Software Ltda.",
+    datePT: "Ago, 2023 - O momento",
+    dateEN: "Ago, 2023 - Now",
+    positionPT: "Desenvolvedor Frontend",
+    positionEN: "Frontend Developer",
+    descriptionPT:
+      "Atualmente, sou desenvolvedor frontend na Cartsys Software Ltda., onde trabalho com TypeScript e o framework Next.js em projetos de software voltados para cartórios. Além de contribuir com o desenvolvimento, também auxilio e lidero a equipe de frontend, garantindo a entrega de soluções eficientes e inovadoras para nossos clientes.",
+    descriptionEN:
+      "Currently, I am a frontend developer at Cartsys Software Ltda., where I work with TypeScript and the Next.js framework on software projects for notary offices. In addition to contributing to development, I also assist and lead the frontend team, ensuring the delivery of efficient and innovative solutions for our clients.",
+  },
+  {
+    company: "Freelancer Dev.",
+    datePT: "Fev, 2022 - Ago, 2023",
+    dateEN: "Feb, 2022 - Ago, 2023",
+    positionPT: "Desenvolvedor Fullstack",
+    positionEN: "Fullstack Developer",
+    descriptionPT:
+      "Desenvolvi uma variedade de aplicações, incluindo apps de delivery, correções em sistemas legados e aplicações mobile, desktop e web. Utilizo TypeScript no frontend com Next.js e React, e no backend com Node.js. Além disso, trabalho com React Native para desenvolvimento mobile, garantindo soluções robustas e eficientes para meus clientes.",
+    descriptionEN:
+      "As a freelancer, I develop a variety of applications, including delivery apps, legacy system corrections, and mobile, desktop, and web applications. I use TypeScript on the frontend with Next.js and React, and on the backend with Node.js. Additionally, I work with React Native for mobile development, ensuring robust and efficient solutions for my clients.",
+  },
+  {
+    company: "Procipa Ltda.",
+    datePT: "Jan, 2022 - Abr, 2022",
+    dateEN: "Jan, 2022 - Apr, 2022",
+    positionPT: "Gerente de Setor",
+    positionEN: "Sector Manager",
+    descriptionPT:
+      "Atuei no acabamento de artefatos de couro para EPIs, liderando o setor de empacotamento e amarração. Minhas responsabilidades incluíam garantir a qualidade final dos produtos e coordenar as atividades de embalagem e preparação para envio.",
+    descriptionEN:
+      "I worked in finishing leather artifacts for PPE, leading the packaging and tying department. My responsibilities included ensuring the final quality of products and coordinating packaging activities for shipment.",
+  },
+  {
+    company: "Exército Brasileiro",
+    datePT: "Fev, 2021 - Jan, 2022",
+    dateEN: "Feb, 2022 - Jan, 2022",
+    positionPT: "Soldado EV",
+    positionEN: "EV Soldier",
+    descriptionPT:
+      "Servi como Soldado EV Corneteiro no 4° Batalhão de Engenharia de Combate de Itajubá/MG, onde desempenhei funções essenciais na comunicação e organização interna da unidade.",
+    descriptionEN:
+      "Served as an EV Soldier Bugler at the 4th Combat Engineering Battalion in Itajubá/MG, performing essential roles in unit communication and internal organization",
+  },
+  {
+    company: "Prefeitura de Cristina/MG",
+    datePT: "Fev, 2020 - Dez, 2020",
+    dateEN: "Feb, 2020 - Dec, 2020",
+    positionPT: "Professor de Música",
+    positionEN: "Music Teacher",
+    descriptionPT:
+      "Fui professor de Musicalização Infantil e construção de instrumentos com materiais recicláveis em um projeto da Prefeitura de Cristina, Minas Gerais. Meu papel incluía desenvolver atividades educativas e criativas para crianças, promovendo a sensibilidade musical e a sustentabilidade.",
+    descriptionEN:
+      "Taught Children's Music Education and instrument building with recycled materials in a project sponsored by the Municipality of Cristina, Minas Gerais. My role involved designing educational and creative activities for children, fostering musical sensitivity and sustainability.",
+  },
+];

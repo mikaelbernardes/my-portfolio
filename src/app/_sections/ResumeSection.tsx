86bc2fc632d286c147d420f9c73471db38979632
Mikael <bernardesmikael@outlook.com>
2024-06-27 21:08:04 -0300

    feat: add TimeLine component in Resume Section

---
+++ src/app/_sections/ResumeSection.tsx
@@ -0,0 +1,57 @@
+import { FaHouse } from "react-icons/fa6";
+
+import { SectionComponent } from "@/components/SectionComponent";
+import { TimelineTemplate } from "@/components/TimeLine/template/TimeLineTemplate";
+import { useLanguageStore } from "@/factory/STORE_FACTORY/impls";
+
+import { experiencesData } from "./ResumeData";
+
+function ResumeSection() {
+  const { language } = useLanguageStore();
+
+  return (
+    <SectionComponent
+      id="home"
+      icon={FaHouse}
+      title={language === "en" ? "Resume" : "Currículo"}
+    >
+      <div className="sm:text-[28px] md:text-[56px] lg:text-[56px] txt-100 font-light">
+        {language === "en" ? (
+          <h2>
+            Professional{" "}
+            <span className="text-Primary font-semibold">Experience</span>
+          </h2>
+        ) : (
+          <h2>
+            Experiência{" "}
+            <span className="text-Primary font-semibold">Profissional</span>
+          </h2>
+        )}
+      </div>
+      {language === "en" ? (
+        <p className="txt-300">
+          <span className="txt-100">Specialist in software development</span>,
+          experienced leader, and highly qualified.{" "}
+          <span className="txt-100">Strong in team leadership</span> and{" "}
+          <span className="txt-100">complex project management</span>,
+          consistently delivering{" "}
+          <span className="txt-100">high-quality results</span>.
+        </p>
+      ) : (
+        <p className="txt-300">
+          <span className="txt-100">
+            Especialista em desenvolvimento de software
+          </span>
+          , líder experiente e altamente qualificado.{" "}
+          <span className="txt-100">Forte em liderança de equipes</span> e{" "}
+          <span className="txt-100">gestão de projetos complexos</span>, sempre
+          entregando{" "}
+          <span className="txt-100">resultados de alta qualidade</span>.
+        </p>
+      )}
+      <TimelineTemplate data={experiencesData} />
+    </SectionComponent>
+  );
+}
+
+export { ResumeSection };
